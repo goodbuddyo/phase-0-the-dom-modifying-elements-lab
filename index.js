5110e2572ff8270d34a6ca7cb02cf756a3ad28f3
Seano <sean01234@yahoo.com>
2023-04-11 22:03:28 -0400

    update dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+const elementMain = document.getElementById("main");
+elementMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Freddy Mercury says we is the champion";
